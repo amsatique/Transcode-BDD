a7620e0cab5802de4ed104b296295fd68c1bc655
amsatique <amsatique@gmail.com>
2016-03-05 14:57:42 +0100

    Change pushbullet message

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 #Pushbullet notifications
-echo "Container succefully redeployed : ${TUTUM_NODE_HOSTNAME} - ${TUTUM_SERVICE_HOSTNAME} - ${TUTUM_CONTAINER_HOSTNAME}" >> /var/log/messages
+echo "Container succefully redeployed : ${TUTUM_STACK_NAME} - ${TUTUM_SERVICE_HOSTNAME} - ${TUTUM_CONTAINER_HOSTNAME}" >> /var/log/messages
 /pushbullet.sh
 
 exec "$@"
